ccaf33dc995b483f3977784646a5fda0e448c762
Koditinho <victoriafabifrj@gmail.com>
2026-08-19 21:21:28 -0300

    Acabdie

---
+++ Primeiro Bimestre/Atividade 1/Exemplo 2/Projeito/App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+<View style={{ padding: 20 }}>
+<Text style={{ fontSize: 24 }}>
+Bem-vindo!
+</Text>
+</View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
